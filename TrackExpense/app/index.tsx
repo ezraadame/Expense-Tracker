bda965fcf003bb2e92d79bcaff7b4c18dfab70d5
ezraadame <oscaroadame@gmail.com>
2025-12-29 13:14:20 -0600

    Reset app with available js script. Moved beginning app into app-example

---
+++ TrackExpense/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>I have reset the project using the script available - Ezra</Text>
+    </View>
+  );
+}
